33dfc0609359d91b525317037d36021c66700a0e
Daniel Trejo Banos <daniel.trejobanos@sdsc.ethz.ch>
2025-08-06 17:06:36 +0200

    fix: remove horizontal flip of sagittal data in visualisations.py

--- maldi/visualisations.py
+++ maldi/visualisations.py
@@ -186,8 +186,8 @@ for cur_file in tqdm(files, desc="Processing volumes"):
         # Get the sagittal slice and flip it horizontally
         sagittal_data = volume[:, :, x_pos]
         # Flip the sagittal data along the horizontal axis (left-right flip)
-        sagittal_data = np.fliplr(sagittal_data)
-        
+        #sagittal_data = np.fliplr(sagittal_data)
+
         sagittal_layer = viewer.add_image(
             sagittal_data,
             name=f'sagittal_slice_{i}',
